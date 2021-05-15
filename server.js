8f3a115fdde17cf22efae845919d6baa7287fefa
valentina comerci <valentinacomerci3@gmail.com>
2021-05-15 11:52:03 -0700

    assignment week 1

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 const hostname = 'localhost';
 const port = 3000;
+
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/partners', partnerRouter);
+app.use('/promotions', promotionRouter);
 
 app.use(express.static(__dirname + '/public'));
 
